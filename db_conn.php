a63de19b39d7c7f9651f312474ab9b6b481d8f72
Laith Almomani <113889731+almlaith@users.noreply.github.com>
2023-09-15 22:34:12 +0300

    Add files via upload
    Main Page.

---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+$sarvername="localhost";
+$uasername="root";
+$pass="";
+$db="final";
+
+$conn=new mysqli($sarvername,$uasername,$pass,$db);
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,88 @@
+
+
+<?php 
+
+session_start(); 
+
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+    function validate($data){
+
+       $data = trim($data);
+
+       $data = stripslashes($data);
+
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $uname = validate($_POST['uname']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($uname)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM user WHERE username='$uname' AND pass='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['username'] === $uname && $row['pass'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['name'] = $row['username'];
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['admin'] = $row['roleid'];
+
+                $_SESSION['adminname'] = $row['adminname'];
+
+                if($_SESSION['admin']  == 1){header("Location: http://localhost/final/admin/home.php");}               
+                else header("Location: http://localhost/final/user/home.php");
+               
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
+
+}
